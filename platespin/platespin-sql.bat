e258ff1374ecd5c1c174ced2f6dd63add819e222
sonalnikam <sonal.nikam@atos.net>
2017-11-24 14:28:56 +0530

    Update platespin-sql.bat

--- platespin/platespin-sql.bat
+++ platespin/platespin-sql.bat
@@ -4,7 +4,7 @@ C:\Windows\System32\inetsrv\appcmd set config /section:WindowsAuthentication /en
 iisreset
 C:\Users\Administrator\Downloads\PlateSpinMigrateSetup\PlateSpinImage\ServerInstall\setup.exe /s /v/qn /v"/l*v c:\install_windowsAuth_5.log" ^
 /v"DBINSTALLTYPE=customerinstall" ^
-/v"IS_SQLSERVER_SERVER=WIN04\WIN04" ^
+/v"IS_SQLSERVER_SERVER=(local)\SQLEXPRESS" ^
 /v"IS_SQLSERVER_AUTHENTICATION=1" ^
 /v"IS_SQLSERVER_PASSWORD=Atos@123"
 
